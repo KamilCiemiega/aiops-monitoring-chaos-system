31727546ff28cdea66415e295590403d16d19da0
Kamil <kamil.qciemiega@gmail.com>
2026-02-10 23:56:23 +0100

    Setup: Java 21, Maven, Docker Compose with Postgres/InfluxDB/Grafana

---
+++ src/main/java/pl/kamil/aiops/MonitoringChaosSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.kamil.aiops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringChaosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringChaosSystemApplication.class, args);
+	}
+
+}
